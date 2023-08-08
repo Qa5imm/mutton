6f600eca0c98c0292e9ba239add3bb14f05bc334
Qa5im <24100256@lums.edu.pk>
2023-08-08 18:28:26 +0500

    added MapperController

---
+++ app/Http/Controllers/Mappers/MapperController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers\Mappers;
+
+use App\Http\Controllers\Mappers\AirSial\AirSialController;
+use App\Http\Controllers\Mappers\Aljazeera\AljazeeraController;
+
+class MapperController
+{
+    protected $mappedData = [];
+    protected $data = [];
+    public function getData()
+    {
+        $response = file_get_contents("./api.json");
+        $data = json_decode($response, true);
+        $this->mappedData[] = AljazeeraController::getAirlineData($data);
+        $this->mappedData[] = AirSialController::getAirlineData($data);        
+        // dd($this->mappedData[0]);
+        return response()->json(['data' => $this->mappedData]);
+        // return response()->json(['message' => 'here']);
+
+    }
+}
