d533d9ddfbe03d0a76367728b15f98346dec014a
Qa5im <24100256@lums.edu.pk>
2023-08-08 11:48:09 +0500

    utils folder added

---
+++ app/Http/Controllers/Mappers/utils/utils.php
@@ -0,0 +1,18 @@
+<?php
+
+use Carbon\Carbon;
+
+function formatDate($timeDiff)
+{
+    $hours = $timeDiff->h;
+    $minutes = $timeDiff->i;
+    return $hours . "h " . $minutes . "m";
+}
+
+function calculateTimeDuration($dep, $arr)
+{
+    $startDate = Carbon::parse($dep);
+    $endDate = Carbon::parse($arr);
+    $timeDifference = $startDate->diff($endDate);
+    return formatDate($timeDifference);
+}
