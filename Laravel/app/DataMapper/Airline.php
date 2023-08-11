0cd046b166adcbf7109a1d919dbeb8cb8bde700e
Qa5im <24100256@lums.edu.pk>
2023-08-11 07:42:37 +0500

    type check added

--- Laravel/app/DataMapper/Airline.php
+++ Laravel/app/DataMapper/Airline.php
@@ -3,12 +3,28 @@
 namespace App\DataMapper;
 
 use App\DataMapper\Flight;
+use Illuminate\Support\Str;
 use Exception;
 use JsonSerializable;
+use PDO;
+
+function formatClassName($name)
+{
+    $singular = Str::Singular($name);
+    $capitalizedName = Str::ucfirst($singular);
+    return $capitalizedName;
+}
+function classNameResolutor($name)
+{
+    $formattedName = formatClassName($name);
+    $class = "\App\DataMapper\\" . $formattedName;
+    return $class;
+}
 
 class Airline implements JsonSerializable
 {
     protected $flights = [];
+
     public function __construct(
         protected String $name,
         protected String $logo,
@@ -23,7 +39,12 @@ class Airline implements JsonSerializable
     public function __set($name, $value)
     {
         if (property_exists($this, $name)) {
-            $this->$name[] = $value;
+            $class = classNameResolutor($name);
+            if ($value instanceof $class) {
+                $this->$name[] = $value;
+            } else {
+                throw new Exception("Invalid Assignment");
+            }
         } else {
             throw new Exception("Property you're trying to set doesn't exist");
         }
